681ec376e98f3e222c9122372c719fc780018b08
mykola-lp <mykola.sukhorab@gmail.com>
2024-08-13 14:10:02 +0300

    Practice 1.

---
+++ NIX/JavaScript/begginer/js-tasks/scripts/task01.js
@@ -0,0 +1,23 @@
+/* task01.js */
+function task01() {
+    const camelCase = 0
+        , snake_case = 1
+        , PascalCase = 3;
+  
+    alert(camelCase);
+    console.log(camelCase);
+  
+    alert(snake_case);
+    console.log(snake_case);
+  
+    alert(PascalCase);
+    console.log(PascalCase);
+  
+    /*
+     `alert` method is opening pop-up window with message and to continue 
+             manipulate with your browser you should first close it.
+  
+     `console.log` method is working in specific tab - Console. To interact 
+                   with that tab you must first close `alter` window.        
+    */
+};
